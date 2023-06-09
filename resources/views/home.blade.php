77016aaf56848db618242aef3e8659fb23766d23
sajjad <mynameissajjad@gmail.com>
2023-06-09 15:46:54 +0330

    maintain old values in input fields

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,25 +6,35 @@
 <form action="/register" method="post">
   @csrf
   <label>Username : </label>
-  <input id="username" name="username" />
+  <input id="username" name="username" value="{{ old('username') }}" />
   @error('username')
     <span>{{$message}}</span>
   @enderror
   <br />
   <label>Email : </label>
-  <input id="email" name="email" />
+  <input id="email" name="email" value="{{ old('email') }}" />
   @error('email')
     <span>{{$message}}</span>
   @enderror
   <br />
   <label>Password : </label>
-  <input id="password" name="password" />
+  <input 
+  type="password"
+  id="password" 
+  name="password" 
+  value="{{ old('password') }}" 
+  />
   @error('password')
     <span>{{$message}}</span>
   @enderror
   <br />
   <label>Password Confirmation : </label>
-  <input id="password_confirmation" name="password_confirmation" />
+  <input 
+  type="password"
+  id="password_confirmation" 
+  name="password_confirmation" 
+  value="{{ old('password_confirmation') }}"  
+  />
   @error('password_confirmation')
     <span>{{$message}}</span>
   @enderror
