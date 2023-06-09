6ba67e82b233e80c8cf1e82a260363afcc2a5bbb
sajjad <mynameissajjad@gmail.com>
2023-06-09 13:14:38 +0330

    add UserController and add register form to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
 <p>Welcome to Laravel</p>
+
+<form action="" method="post">
+  @csrf
+  <label>Email : </label>
+  <input id="email" name="email" />
+  <p></p>
+  <label>Password : </label>
+  <input id="password" name="password" />
+</form>
+
 @endsection
+
+
