35c69650a24b7f0dc72b7813282c13d91e8f0f7a
sajjad <mynameissajjad@gmail.com>
2023-06-09 14:42:12 +0330

    send data to registration controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function register(Request $request){
-      $username = $request['username'];
-      return "$username registered successfully";
+      
+      $inputData = $request->validate([
+        'username' => 'required' ,
+        'email' => 'required' ,
+        'password' => 'required' ,
+      ]);
+
+      User::create([
+        'username' => $inputData['username'] ,
+        'email' => $inputData['email'] ,
+        'password' => $inputData['password'] ,
+      ]);
+      
+      return $inputData['username'] . " registered successfully with email " . $inputData['email'] . " and password " . $inputData['password'];
     }
 }
