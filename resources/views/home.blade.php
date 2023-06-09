d7d0be72e24b92e4c26291893836e58afe3300a1
sajjad <mynameissajjad@gmail.com>
2023-06-09 15:34:49 +0330

    add validation errors to registeration page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,21 @@
   @csrf
   <label>Username : </label>
   <input id="username" name="username" />
+  @error('username')
+    <span>{{$message}}</span>
+  @enderror
   <br />
   <label>Email : </label>
   <input id="email" name="email" />
+  @error('email')
+    <span>{{$message}}</span>
+  @enderror
   <br />
   <label>Password : </label>
   <input id="password" name="password" />
+  @error('password')
+    <span>{{$message}}</span>
+  @enderror
   <br />
   <button>Register</button>
 </form>
